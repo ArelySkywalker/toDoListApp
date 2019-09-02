2b4a64ffa9f0877787f0acbefbf22c9dff18987d
Arely Miramontes <arelymts@gmail.com>
2019-09-02 11:32:39 -0500

    delete and tick a task

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ loadEvents();
 // load every event on the page
 function loadEvents() {
 	document.querySelector('form').addEventListener('submit', submit);
-	// recently added
 	document.getElementById('clear').addEventListener('click', clearList);
+	// recently added
+	document.querySelector('ul').addEventListener('click', deleteOrTick);
 }
 
 // submit data function
@@ -29,4 +30,32 @@ function addTask(task) {
 function clearList(e) {
 	// setting the ul elements to an empty string
 	let ul = document.querySelector('ul').innerHTML = '';
+}
+
+// deleteTick
+function deleteOrTick(e) {
+	if(e.target.className == 'delete') {
+		deleteTask(e);
+	}else {
+		tickTask(e);
+	}
+}
+
+// delete task
+function deleteTask(e) {
+	let remove = e.target.parentNode;
+	let parentNode = remove.parentNode;
+	parentNode.removeChild(remove);
+}
+
+// tick a task
+function tickTask(e) {
+	const task = e.target.nextSibling;
+	if(e.target.checked) {
+		task.style.textDecoration = 'line-through';
+		task.style.color = '#ff0000';
+	}else {
+		task.style.textDecoration = 'none';
+		task.style.color = '#2f4f4f';
+	}
 }
